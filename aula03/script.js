a76a14a5d6dd4abe33f4bf04f110dd26c02f8a7a
Tiago Oliveira <67693562+Tiagoksio@users.noreply.github.com>
2021-03-25 19:23:00 -0300

    Add joquempô mais mais

--- aula03/script.js
+++ aula03/script.js
@@ -1,6 +1,5 @@
 var jogadorPts = 0
 var computadorPts = 0
-var maoJogador = document.getElementById('maoJogador')
 var botJogar = document.getElementById('botJogar')
 var botRegras = document.getElementById('botRegras')
 
@@ -27,52 +26,68 @@ function mudarImagem(opt, ident) {
     }
 }
 function maoPc() {
-    let sorteio = parseInt(Math.random().toFixed(1) * 10 / 2)
+    let sorteio = parseInt((Math.floor(Math.random() * 10) + 1)/2)
     switch (sorteio) {
         case 1:
+            mudarImagem('pedra', 'mao-pc')
             return 'pedra'
         case 2:
+            mudarImagem('papel', 'mao-pc')
             return 'papel'
         case 3:
+            mudarImagem('tesoura', 'mao-pc')
             return 'tesoura'
         case 4:
+            mudarImagem('lagarto', 'mao-pc')
             return 'lagarto'
         case 5:
+            mudarImagem('spock', 'mao-pc')
             return 'spock'
-        default:
-            maoPc()
-            break;
     }
 }
 function joquempo(maoJog, maoPc) {
     if (maoJog == 'pedra' && (maoPc == 'tesoura' || maoPc == 'lagarto')) {
-        jogadorPts++
+        document.getElementById('placar').innerHTML = `${++jogadorPts} VS ${computadorPts}<hr><p>Player PC</p>` 
     } else if (maoJog == 'papel' && (maoJog == 'pedra' || maoPc == 'spock')) {
-        jogadorPts++
+        document.getElementById('placar').innerHTML = `${++jogadorPts} VS ${computadorPts}<hr><p>Player PC</p>`
     } else if (maoJog == 'tesoura' && (maoPc == 'papel' || maoPc == 'lagarto')) {
-        jogadorPts++
+        document.getElementById('placar').innerHTML = `${++jogadorPts} VS ${computadorPts}<hr><p>Player PC</p>`
     } else if (maoJog == 'lagarto' && (maoPc == 'spock' || maoPc == 'papel')) {
-        jogadorPts++
+        document.getElementById('placar').innerHTML = `${++jogadorPts} VS ${computadorPts}<hr><p>Player PC</p>`
     } else if (maoJog == 'spock' && (maoPc == 'tesoura' || maoPc == 'pedra')) {
-        jogadorPts++
+        document.getElementById('placar').innerHTML = `${++jogadorPts} VS ${computadorPts}<hr><p>Player PC</p>`
     } else if (maoJog != maoPc){
-        computadorPts++
+        document.getElementById('placar').innerHTML = `${jogadorPts} VS ${++computadorPts}<hr><p>Player PC</p>`
     } else {
-        return 'empate'
+        document.getElementById('placar').innerHTML = `${jogadorPts} VS ${computadorPts}<hr><p>Player PC</p>`
     }
 }
 function jogar() {
-    var maoComputador = maoPc()
-    mudarImagem(maoJogador.value, 'mao-jogador')
-    mudarImagem(maoComputador, 'mao-pc')
+    let maoJogador = document.getElementById('maoJogador')
+    let maoComputador = maoPc()
     joquempo(maoJogador.value, maoComputador)
-    if (jogadorPts == 2 || computadorPts == 2 ) {
-        alert("GAME OVER")
-        jogadorPts = 0
-        computadorPts = 2
-    }
+    mudarImagem(maoJogador.value, 'mao-jogador')
     
+    if (jogadorPts == 2) {
+        confirm('Parabéns')
+        jogadorPts = 0
+        computadorPts = 0
+    } else if (computadorPts == 2) {
+        confirm('Não foi dessa vez')
+        jogadorPts = 0
+        computadorPts = 0
+    }
 }
 function regras() {
-    alert('regras')
+    alert(`Regras
+    Tesoura corta Papel
+    Papel cobre Pedra
+    Pedra esmaga Lagarto
+    Lagarto envenena Spock
+    Spock quebra Tesoura
+    Tesoura decapita Lagarto
+    Lagarto come Papel
+    Papel desmente Spock
+    Spock destrói Pedra
+    Pedra quebra Tesoura`)
 }
